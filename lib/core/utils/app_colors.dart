2f90a6a43f69d5b04ca2782ca8fd18b6f9fa9ac8
Subhan Janura <subhan.j@mhs.unsyiah.ac.id>
2023-11-08 10:12:23 +0700

    Update app_colors.dart

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color kBlueColor = Color(0xff4E73AF);
+  static const Color softWhite = Color(0xFFF3F9FB);
+  static const Color softBlue = Color(0xFFA0BEE0);
+  static const Color primaryBlue = Color(0xFF4E73AF);
+  static const Color darkBlue = Color(0xFF1C3557);
 }
