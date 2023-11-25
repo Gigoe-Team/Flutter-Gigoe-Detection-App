1b8e801a53175fafe1efcd1a1dbf275c164f0ced
Subhan Janura <subhan.j@mhs.unsyiah.ac.id>
2023-11-25 15:35:56 +0700

    Update splash_page.dart

--- lib/features/presentation/pages/splash_page.dart
+++ lib/features/presentation/pages/splash_page.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:gigoe_detection_app/core/utils/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -14,7 +16,7 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-    Timer(const Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 5), () {
       User? user = FirebaseAuth.instance.currentUser;
 
       if (user == null) {
@@ -31,35 +33,46 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFA0BEE0),
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/logo2.png',
-                  width: 100,
-                  height: 100,
-                ),
-              ],
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              AppColors.softBlue,
+              AppColors.darkBlue,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              margin: const EdgeInsets.only(bottom: 20),
-              child: const Text(
-                'Versi 1.0.1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
+        ),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo2.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 50),
+                child: Text(
+                  'Versi 1.0.1',
+                  style: GoogleFonts.poppins(
+                    color: AppColors.softWhite,
+                    fontSize: 16,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
